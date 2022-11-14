b7186d84924056424ec91387dd67908113f46205
niccolo03 <117930767+niccolo03@users.noreply.github.com>
2022-11-14 14:44:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,9 +2,8 @@
 using namespace std;
 
 int main() {
-    float a=0, b=0, x=0, y=0;
+    float a=0, b=0, x=0, y=0, z=0;
     cin >> a >> b >> x >> y;
-    float z=0;
     if(x<0, y>0){
         z=(a*x)-(b*y);
     }
@@ -14,5 +13,5 @@ int main() {
         z=(a*x)+(b*y*y);
     }
    cout << z << endl;
-    return 0;
 }
+return 0;
