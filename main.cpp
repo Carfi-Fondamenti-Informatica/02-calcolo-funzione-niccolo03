bea4b2da13fb1b329f15e881e5fcdc8f7a48b286
niccolo03 <117930767+niccolo03@users.noreply.github.com>
2022-11-14 14:42:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,12 +6,12 @@ int main() {
     cin >> a >> b >> x >> y;
     float z=0;
     if(x<0, y>0){
-        z= a*x-b*y;
+        z=(a*x)-(b*y);
     }
     else if(x>=0, y<=0){
-        z=a*(x*x)-b*y;
+        z=(a*x*x)-(b*y*y);
     } else{
-        z=a*x+b*(y*y);
+        z=(a*x)+(b*y*y);
     }
    cout << z << endl;
     return 0;
